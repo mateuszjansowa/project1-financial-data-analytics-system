9136058ce7560b797425760a5dd01004087a4616
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-01-16 19:21:09 +0100

    CR fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,18 @@ import service.ExpenseProcessor;
 import service.ReportGenerator;
 import util.CsvReader;
 
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
-    private static final String input = "D:\\coding\\Java\\FinancialDataAnalyticsSystem\\src\\main\\resources\\input\\expenses.csv";
+    private static final String INPUT_FILE = Paths.get("src", "main", "resources", "input", "expenses.csv").toString();
+    private static final String CATEGORY_OUTPUT = Paths.get("src", "main", "resources", "outpus", "categoryData.json").toString();
+    private static final String HIGH_SPENDERS_OUTPUT = Paths.get("src", "main", "resources", "output", "highSpenders.txt").toString();
 
     public static void main(String[] args) {
         try {
-            List<Client> clients = CsvReader.readCsv(input);
+            List<Client> clients = CsvReader.readCsv(INPUT_FILE);
             ExpenseProcessor expenseProcessor = new ExpenseProcessor(clients);
 
             for (Client client : clients) {
@@ -28,8 +31,8 @@ public class Main {
             }
 
             ReportGenerator raportGenerator = new ReportGenerator(categoryData, clients);
-            raportGenerator.generateCategoryReport("D:\\coding\\Java\\FinancialDataAnalyticsSystem\\src\\main\\resources\\output\\categoryData.json");
-            raportGenerator.generateHighSpendersReport("D:\\coding\\Java\\FinancialDataAnalyticsSystem\\src\\main\\resources\\output\\highSpenders.txt");
+            raportGenerator.generateCategoryReport(CATEGORY_OUTPUT);
+            raportGenerator.generateHighSpendersReport(HIGH_SPENDERS_OUTPUT);
         } catch (Exception e) {
             System.out.println("Error while executing Main class: " + e.getMessage());
             e.printStackTrace();
